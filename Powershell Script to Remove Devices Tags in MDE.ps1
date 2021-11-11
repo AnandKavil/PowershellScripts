4e3e53ca4487c422222a2b2c69ea8bd12eece51b
AnandKavil <62881775+AnandKavil@users.noreply.github.com>
2021-11-11 13:25:33 +0530

    Add files via upload

---
+++ Powershell Script to Remove Devices Tags in MDE.ps1
@@ -0,0 +1,47 @@
+﻿###################################################
+# The Script will Remove tags from MDE using API  #
+###################################################
+#Paste your tenant ID,app ID,app keys
+#This will create and Store auth token for future use
+$tenantId = ‘’ #Paste Your Tenant ID
+$appId = ‘’    #paste Your App ID
+$appSecret = ‘’  #paste your App Key
+$resourceAppIdUri = 'https://api.securitycenter.microsoft.com'
+$oAuthUri = "https://login.microsoftonline.com/$tenantId/oauth2/token"
+$authBody = [Ordered] @{
+   resource = “$resourceAppIdUri”
+    client_id = “$appId”
+    client_secret = “$appSecret”
+    grant_type = ‘client_credentials’
+}
+$authResponse = Invoke-RestMethod -Method Post -Uri $oAuthUri -Body $authBody -ErrorAction Stop
+$token = $authResponse.access_token
+$headers = @{
+        ‘Content-Type’ = ‘application/json’
+        Accept = ‘application/json’
+        Authorization = “Bearer $token”
+    }
+# Clean variables
+$Data = @();
+$MachineName = $null;
+$MachineTag = $null;
+$MachineId = $null;
+$Data = Import-Csv -Path C:\Users\anandp\Desktop\test_tag.csv
+# Remove Tag as per the input file  
+$Data | foreach {
+    $MachineName = $($_.Name);
+    $MachineTag = $($_.Tag);
+    $url = “https://api.securitycenter.microsoft.com/api/machines/$MachineName" 
+    $webResponse = Invoke-RestMethod -Method Get -Uri $url -Headers $headers -ErrorAction Stop
+    $MachineId = $webResponse.id;
+    $body = @{
+      “Value”=$MachineTag;
+      “Action”=”Remove”;
+    }
+    $url = “https://api.securitycenter.microsoft.com/api/machines/$MachineId/tags” 
+    $webResponse = Invoke-WebRequest -Method Post -Uri $url -Headers $headers -Body ($body|ConvertTo-Json) -ContentType “application/json” -ErrorAction Stop
+    # Clean variables 
+    $MachineName = $null;
+    $MachineTag = $null;
+    $MachineId = $null;
+}
